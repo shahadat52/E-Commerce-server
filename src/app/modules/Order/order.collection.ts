ed58d2ecc86fe32397ae07996ed397b6a1905380
shahadat52 <shahadathossain.sh255@gmail.com>
2024-07-04 22:58:46 -0600

    order module refactor

--- src/app/modules/Order/order.collection.ts
+++ src/app/modules/Order/order.collection.ts
@@ -2,34 +2,44 @@
 import { Request, Response } from 'express';
 import { orderServices } from './order.service';
 import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
 
 const createOrder = catchAsync(async (req: Request, res: Response) => {
   const result: any = await orderServices.createOrderInDB(req.body);
 
-  res.status(200).json({
+  sendResponse(res, {
+    statusCode: 200,
     success: true,
     message: result?.message || 'Order created successfully!',
     data: result,
-  });
+  })
 });
 
-const getAllOrders = async (req: Request, res: Response) => {
+const getAllOrders = catchAsync(async (req: Request, res: Response) => {
   const result = await orderServices.getAllOrdersFromDB();
   res.status(200).json({
+
+  });
+
+  sendResponse(res, {
+    statusCode: 200,
     success: true,
     message: 'Orders fetched successfully!',
     data: result,
-  });
-};
+  })
+});
 
-const getOrder = async (req: Request, res: Response) => {
+const getOrder = catchAsync(async (req: Request, res: Response) => {
   const result = await orderServices.getOrderFromDB(req.query);
-  res.status(200).json({
+
+  sendResponse(res, {
+    statusCode: 200,
     success: true,
     message: 'Orders fetched successfully for user email!',
     data: result,
-  });
-};
+  })
+
+});
 
 export const orderCollections = {
   createOrder,
