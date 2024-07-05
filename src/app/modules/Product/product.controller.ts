7572657aadf0609b74851a5a9cd856a44bfaa807
shahadat52 <shahadathossain.sh255@gmail.com>
2024-07-04 23:04:50 -0600

    product module refactor

--- src/app/modules/Product/product.controller.ts
+++ src/app/modules/Product/product.controller.ts
@@ -1,59 +1,65 @@
 import { Request, Response } from 'express';
 import { productServices } from './product.service';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
 
-const createProduct = async (req: Request, res: Response) => {
+const createProduct = catchAsync(async (req: Request, res: Response) => {
   const result = await productServices.createProductInDB(req.body);
 
-  res.status(200).json({
+  sendResponse(res, {
+    statusCode: 200,
     success: true,
     message: 'Product created successfully!',
     data: result,
-  });
-};
+  })
+});
 
-const getAllProducts = async (req: Request, res: Response) => {
+const getAllProducts = catchAsync(async (req: Request, res: Response) => {
   const result = await productServices.getAllProductsFromDB(req.query);
 
-  res.status(200).json({
+  sendResponse(res, {
+    statusCode: 200,
     success: true,
     message: 'Products fetched successfully!',
     data: result,
-  });
-};
+  })
+});
 
-const getSingleProduct = async (req: Request, res: Response) => {
+const getSingleProduct = catchAsync(async (req: Request, res: Response) => {
   const { productId } = req.params;
   const result = await productServices.getSingleProductFromDB(productId);
 
-  res.status(200).json({
+  sendResponse(res, {
+    statusCode: 200,
     success: true,
     message: 'Product fetched successfully!',
     data: result,
-  });
-};
+  })
+});
 
-const updateSingleProduct = async (req: Request, res: Response) => {
-  const data = req.body;
+const updateSingleProduct = catchAsync(async (req: Request, res: Response) => {
   const { productId } = req.params;
-  const result = await productServices.updateSingleProductInDB(data, productId);
+  const result = await productServices.updateSingleProductInDB(req.body, productId);
 
-  res.status(200).json({
+  sendResponse(res, {
+    statusCode: 200,
     success: true,
     message: 'Product updated successfully!',
     data: result,
-  });
-};
+  })
+});
 
-const deleteSingleProduct = async (req: Request, res: Response) => {
+const deleteSingleProduct = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
   await productServices.deleteSingleProductFromDB(id);
 
-  res.status(200).json({
+  sendResponse(res, {
+    statusCode: 200,
     success: true,
     message: 'Product deleted successfully!',
     data: null,
-  });
-};
+  })
+});
 
 export const productCollections = {
   createProduct,
